5b506f7b07c9158882b0af77fbdc89478031c971
Angel Labrada Massó <angelitolabm@gmail.com>
2021-09-22 11:46:48 -0400

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -1,78 +1,39 @@
-import React, { useState } from "react";
-import { DragDropContext } from "react-beautiful-dnd";
-import Column from "./components/Column";
-import { status } from "./constants/mock";
+import React, { memo } from "react";
+import PropTypes from "prop-types";
+import { Droppable } from "react-beautiful-dnd";
+import TaskCard from "./TaskCard";
 
-const onDragEnd = (result, columns, setColumns) => {
-  if (!result.destination) return;
-  const { source, destination } = result;
-
-  if (source.droppableId !== destination.droppableId) {
-    const sourceColumn = columns[source.droppableId];
-    const destColumn = columns[destination.droppableId];
-    const sourceItems = [...sourceColumn.items];
-    const destItems = [...destColumn.items];
-    const [removed] = sourceItems.splice(source.index, 1);
-    destItems.splice(destination.index, 0, removed);
-    setColumns({
-      ...columns,
-      [source.droppableId]: {
-        ...sourceColumn,
-        items: sourceItems
-      },
-      [destination.droppableId]: {
-        ...destColumn,
-        items: destItems
-      }
-    });
-  } else {
-    const column = columns[source.droppableId];
-    const copiedItems = [...column.items];
-    const [removed] = copiedItems.splice(source.index, 1);
-    copiedItems.splice(destination.index, 0, removed);
-    setColumns({
-      ...columns,
-      [source.droppableId]: {
-        ...column,
-        items: copiedItems
-      }
-    });
-  }
+const Column = ({ droppableId, column }) => {
+  return (
+    <Droppable droppableId={droppableId} key={droppableId}>
+      {(provided, snapshot) => {
+        return (
+          <div
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+            style={{
+              background: snapshot.isDraggingOver ? "lightblue" : column.color,
+              padding: 4,
+              width: 250,
+              minHeight: 500,
+              border: "2px dashed #ccc",
+              borderRadius: "4px"
+            }}
+          >
+            {column?.items?.map((item, index) => {
+              return <TaskCard key={item.id} item={item} index={index} />;
+            })}
+            {provided.placeholder}
+          </div>
+        );
+      }}
+    </Droppable>
+  );
 };
 
-function App() {
-  const [columns, setColumns] = useState(status);
+Column.propTypes = {
+  column: PropTypes.object,
+  droppableId: PropTypes.string
+};
 
-  return (
-    <div style={{ display: "flex", justifyContent: "center", height: "100%" }}>
-      <DragDropContext
-        onDragEnd={(result) => onDragEnd(result, columns, setColumns)}
-      >
-        {Object.entries(columns).map(([columnId, column], index) => {
-          return (
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center"
-              }}
-              key={columnId}
-            >
-              <h2>{column.name}</h2>
-              <div style={{ margin: 8 }}>
-                <Column
-                  droppableId={columnId}
-                  key={columnId}
-                  index={index}
-                  column={column}
-                />
-              </div>
-            </div>
-          );
-        })}
-      </DragDropContext>
-    </div>
-  );
-}
-
-export default App;
+export default memo(Column);
