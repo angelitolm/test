d60ea7adfe2e5fa9f138215a9dde1b69bd01003e
Angel Labrada Massó <angelitolabm@gmail.com>
2021-09-22 11:02:16 -0400

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -1,46 +1,20 @@
-import React, { memo } from "react";
-import PropTypes from "prop-types";
-import { Draggable } from "react-beautiful-dnd";
-import "../styles.css";
-
-function TaskCard({ item, index }) {
-  return (
-    <Draggable key={item.id} draggableId={item.id} index={index}>
-      {(provided, snapshot) => {
-        return (
-          <div
-            ref={provided.innerRef}
-            {...provided.draggableProps}
-            {...provided.dragHandleProps}
-            style={{
-              userSelect: "none",
-              padding: 16,
-              margin: "0 0 8px 0",
-              minHeight: "50px",
-              backgroundColor: snapshot.isDragging ? "#263B4A" : "#456C86",
-              color: "white",
-              borderRadius: "4px",
-              ...provided.draggableProps.style
-            }}
-          >
-            <div className="conten-card">
-              <img
-                src="https://react-beautiful-dnd.netlify.app/static/media/bmo-min.9c65ecdf.png"
-                alt="logo"
-                className="logo"
-              />
-              {item.content}
-            </div>
-          </div>
-        );
-      }}
-    </Draggable>
-  );
+.App {
+  font-family: sans-serif;
+  text-align: center;
 }
 
-TaskCard.propTypes = {
-  index: PropTypes.number,
-  item: PropTypes.object
-};
+.logo {
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  margin-right: 8px;
+  flex-shrink: 0;
+  -webkit-box-flex: 0;
+  flex-grow: 0;
+}
 
-export default memo(TaskCard);
+.conten-card {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
